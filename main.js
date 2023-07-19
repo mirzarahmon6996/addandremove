8bf1bd5a22bb08a6fa6b8d5810d3cd74d1be8965
Mirzarahmon <rahmonraxmatov@gmail.com>
2023-07-19 23:17:23 +0500

    finish

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function addRow() {
-  var table = document.getElementById("myTable");
-  var row = table.insertRow(table.rows.length);
-  var surnameCell = row.insertCell(0);
-  var nameCell = row.insertCell(1);
-  var ageCell = row.insertCell(2);
-  var numberCell = row.insertCell(3);
-  var countryCell = row.insertCell(4);
-  var actionsCell = row.insertCell(5);
+  let table = document.getElementById("myTable");
+  let row = table.insertRow(table.rows.length);
+  let surnameCell = row.insertCell(0);
+  let nameCell = row.insertCell(1);
+  let ageCell = row.insertCell(2);
+  let numberCell = row.insertCell(3);
+  let countryCell = row.insertCell(4);
+  let actionsCell = row.insertCell(5);
   surnameCell.innerHTML = "<th>Mr</th>";
   nameCell.innerHTML = "<th>Alex</th>";
   ageCell.innerHTML = "<th>30</th>";
@@ -16,6 +16,6 @@ function addRow() {
 }
 
 function deleteRow(btn) {
-  var row = btn.parentNode.parentNode;
+  let row = btn.parentNode.parentNode;
   row.parentNode.removeChild(row);
 }
